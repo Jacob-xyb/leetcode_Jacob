e25062f776759e3cdd19db6b161f3b38bcd492af
“Jacob-xyb” <949782197@qq.com>
2021-08-06 17:34:23 +0800

    cpp start

---
+++ cpp/Jx_test.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <algorithm>
+
+// º¯ÊıÉùÃ÷
+void vector_015();
+
+#define test() vector_015()
---
+++ cpp/leetcode_cpp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Jx_test.h"
+using namespace std;
+
+
+
+int main()
+{
+    test();
+
+    system("pause");
+    return 0;
+}
