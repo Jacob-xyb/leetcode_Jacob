086a07b3a65c63affaa73899def48b92aaf8f84c
Jacob-xyb <949782197@qq.com>
2021-08-12 17:54:37 +0800

    solve the merge in *.cpp

--- cpp/Jx_test.h
+++ cpp/Jx_test.h
@@ -5,8 +5,4 @@ using namespace std;
 #include <unordered_map>
 
 // º¯ÊıÉùÃ÷
-<<<<<<< HEAD
 vector<vector<int>> generate(int numRows);
-=======
-vector<int> plusOne_Jx002(vector<int>& digits);
->>>>>>> 84d140782c97f125396d940f1778c8cbeafa85f4
--- cpp/leetcode_cpp.cpp
+++ cpp/leetcode_cpp.cpp
@@ -5,13 +5,8 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
     vector<int> a{ -2,1,-3,4,-1,2,1,-5,4 };
     cout << generate(5)[0][0] << endl;
-=======
-    vector<int> a{ 9 };
-    cout << plusOne_Jx002(a)[0] << endl;
->>>>>>> 84d140782c97f125396d940f1778c8cbeafa85f4
 
     //cout << vector<int>{9, 9, 9} [0] << endl;
     system("pause");
