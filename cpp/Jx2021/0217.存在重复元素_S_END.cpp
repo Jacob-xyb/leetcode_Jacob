d2a0f810cf723366263c9e0517ed62facf5d42c1
“Jacob-xyb” <949782197@qq.com>
2021-08-19 17:33:01 +0800

    update leetcode_cpp

--- cpp/Jx2021/0217.存在重复元素_S_END.cpp
+++ cpp/Jx2021/0217.存在重复元素_S_END.cpp
@@ -47,3 +47,52 @@ bool containsDuplicate_Jx002(vector<int>& nums)
 	}
 	return false;
 }
+
+//超出时间限制
+bool containsDuplicate_Jx003(vector<int>& nums)
+{
+	int len = nums.size();
+	//sort
+	for (int i = 0; i < len - 2; i++)
+	{
+		for (int j = 0; j < len - 1 - i; j++)
+		{
+			if (nums[j] > nums[j + 1])
+			{
+				int temp = nums[j + 1];
+				nums[j + 1] = nums[j];
+				nums[j] = temp;
+			}
+		}
+		//len-2>max-min-1
+		//if ((len - i) - 2 > nums[len - 1 - i] - nums[0] - 1) return true;
+	}
+	for (int i = 0; i < len-1; i++)
+	{
+		if (nums[i] == nums[i+1])
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+//就很离谱，如何实现的？
+//time:44 ms	//memory:22 MB	//rank:24.59%
+bool containsDuplicate_Jx004(vector<int>& nums)
+{
+	set<int> s(nums.begin(), nums.end());
+	return s.size() != nums.size();
+}
+
+//这个更离谱，最快的方法是先排序？
+bool containsDuplicate_Eg001(vector<int>& nums) {
+	sort(nums.begin(), nums.end());
+	int n = nums.size();
+	for (int i = 0; i < n - 1; i++) {
+		if (nums[i] == nums[i + 1]) {
+			return true;
+		}
+	}
+	return false;
+}
--- cpp/Jx_test.h
+++ cpp/Jx_test.h
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 #include <vector>
+#include <set>
 #include <algorithm>
 #include <unordered_map>
 
